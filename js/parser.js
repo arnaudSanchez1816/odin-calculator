ef0fc17893b7f416f32509b61631c1f42ad32c32
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2025-01-10 16:21:30 +0100

    Move notes links into parser.js file

--- js/parser.js
+++ js/parser.js
@@ -7,6 +7,10 @@
 //                | primary ;
 // primary        → NUMBER | "+" NUMBER | "(" expression ")" ;
 
+// https://journal.stuffwithstuff.com/2011/03/19/pratt-parsers-expression-parsing-made-easy/
+// https://github.com/foolnotion/pratt-parser-calculator/tree/main
+// https://pest.rs/book/examples/calculator.html
+
 // https://craftinginterpreters.com/parsing-expressions.html#syntax-errors
 
 const tokens = require("./lexer");
--- js/script.js
+++
@@ -1,6 +0,0 @@
-// https://journal.stuffwithstuff.com/2011/03/19/pratt-parsers-expression-parsing-made-easy/
-// https://github.com/foolnotion/pratt-parser-calculator/tree/main
-// https://pest.rs/book/examples/calculator.html
-
-//https://craftinginterpreters.com/parsing-expressions.html
-//https://github.com/brandly/lox.js/tree/master/src

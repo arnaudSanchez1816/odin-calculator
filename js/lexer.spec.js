9726c3b523a66e6cf9dfab74850b1a0f10668bc4
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2025-01-10 02:15:24 +0100

    Fix broken lexer tests

--- js/lexer.spec.js
+++ js/lexer.spec.js
@@ -4,89 +4,119 @@ describe("tokenize", () => {
     const test1Exp = "1 + 1";
     test(test1Exp, () => {
         const tokens = [
-            {tokenType : "TOKEN_NUMBER", text : "1"},
-            {tokenType : "TOKEN_PLUS", text : "+"},
-            {tokenType : "TOKEN_NUMBER", text : "1"},
-            {tokenType : "TOKEN_EOF", text : ""}
+            new token.Token(token.TOKEN_TYPES.number, "1"),
+            new token.Token(token.TOKEN_TYPES.plus, "+"),
+            new token.Token(token.TOKEN_TYPES.number, "1"),
+            new token.Token(token.TOKEN_TYPES.eof, ""),
         ];
         expect(token.tokenize(test1Exp)).toEqual(tokens);
     });
     const test2Exp = "1 + 12.1 \u00D7 (5\u00F7(9-1))";
     test(test2Exp, () => {
         const tokens = [
-            {tokenType : "TOKEN_NUMBER", text : "1"},
-            {tokenType : "TOKEN_PLUS", text : "+"},
-            {tokenType : "TOKEN_NUMBER", text : "12.1"},
-            {tokenType : "TOKEN_MULTIPLY", text : "\u00D7"},
-            {tokenType : "TOKEN_BRACE_LEFT", text : "("},
-            {tokenType : "TOKEN_NUMBER", text : "5"},
-            {tokenType : "TOKEN_DIVIDE", text : "\u00F7"},
-            {tokenType : "TOKEN_BRACE_LEFT", text : "("},
-            {tokenType : "TOKEN_NUMBER", text : "9"},
-            {tokenType : "TOKEN_MINUS", text : "-"},
-            {tokenType : "TOKEN_NUMBER", text : "1"},
-            {tokenType : "TOKEN_BRACE_RIGHT", text : ")"},
-            {tokenType : "TOKEN_BRACE_RIGHT", text : ")"},
-            {tokenType : "TOKEN_EOF", text : ""}
+            new token.Token(token.TOKEN_TYPES.number, "1"),
+            new token.Token(token.TOKEN_TYPES.plus, "+"),
+            new token.Token(token.TOKEN_TYPES.number, "12.1"),
+            new token.Token(token.TOKEN_TYPES.multiply, "\u00D7"),
+            new token.Token(token.TOKEN_TYPES.brace_left, "("),
+            new token.Token(token.TOKEN_TYPES.number, "5"),
+            new token.Token(token.TOKEN_TYPES.divide, "\u00F7"),
+            new token.Token(token.TOKEN_TYPES.brace_left, "("),
+            new token.Token(token.TOKEN_TYPES.number, "9"),
+            new token.Token(token.TOKEN_TYPES.minus, "-"),
+            new token.Token(token.TOKEN_TYPES.number, "1"),
+            new token.Token(token.TOKEN_TYPES.brace_right, ")"),
+            new token.Token(token.TOKEN_TYPES.brace_right, ")"),
+            new token.Token(token.TOKEN_TYPES.eof, ""),
         ];
         expect(token.tokenize(test2Exp)).toEqual(tokens);
     });
-    const everySymbolsExp = "+-\u00D7\u00F7\u221A^mod%()";
-    test(everySymbolsExp, () => {
-        const tokens = [
-            {tokenType : "TOKEN_PLUS", text : "+"},
-            {tokenType : "TOKEN_MINUS", text : "-"},
-            {tokenType : "TOKEN_MULTIPLY", text : "\u00D7"},
-            {tokenType : "TOKEN_DIVIDE", text : "\u00F7"},
-            {tokenType : "TOKEN_SQRT", text : "\u221A"},
-            {tokenType : "TOKEN_EXPONENT", text : "^"},
-            {tokenType : "TOKEN_MODULO", text : "mod"},
-            {tokenType : "TOKEN_PERCENT", text : "%"},
-            {tokenType : "TOKEN_BRACE_LEFT", text : "("},
-            {tokenType : "TOKEN_BRACE_RIGHT", text : ")"},
-            {tokenType : "TOKEN_EOF", text : ""},
-        ];
-        expect(token.tokenize(everySymbolsExp)).toEqual(tokens);
-    });
     test("Modulo token", () => {
         const tokens = [
-            {tokenType : "TOKEN_MODULO", text : "mod"},
-            {tokenType : "TOKEN_UNKNOWN", text : "me"},
-            {tokenType : "TOKEN_UNKNOWN", text : "d"},
-            {tokenType : "TOKEN_UNKNOWN", text : "mol"},
-            {tokenType : "TOKEN_UNKNOWN", text : "d"},
-            {tokenType : "TOKEN_EOF", text : ""},
+            new token.Token(token.TOKEN_TYPES.mod, "mod"),
+            new token.Token(token.TOKEN_TYPES.unknown, "me"),
+            new token.Token(token.TOKEN_TYPES.unknown, "d"),
+            new token.Token(token.TOKEN_TYPES.unknown, "mol"),
+            new token.Token(token.TOKEN_TYPES.unknown, "d"),
+            new token.Token(token.TOKEN_TYPES.eof, ""),
         ];
         expect(token.tokenize("modmedmold")).toEqual(tokens);
     });
     const allNumbersExp = "123456789987654321 + 00000123";
     test(allNumbersExp, () => {
         const tokens = [
-            {tokenType : "TOKEN_NUMBER", text : "123456789987654321"},
-            {tokenType : "TOKEN_PLUS", text : "+"},
-            {tokenType : "TOKEN_NUMBER", text : "00000123"},
-            {tokenType : "TOKEN_EOF", text : ""},
+            new token.Token(token.TOKEN_TYPES.number, "123456789987654321"),
+            new token.Token(token.TOKEN_TYPES.plus, "+"),
+            new token.Token(token.TOKEN_TYPES.number, "00000123"),
+            new token.Token(token.TOKEN_TYPES.eof, ""),
         ];
         expect(token.tokenize(allNumbersExp)).toEqual(tokens);
     });
 
     test("Decimal numbers", () => {
         expect(token.tokenize("12.56")).toEqual([
-            {tokenType : "TOKEN_NUMBER", text : "12.56"},
-            {tokenType : "TOKEN_EOF", text : ""},
+            new token.Token(token.TOKEN_TYPES.number, "12.56"),
+            new token.Token(token.TOKEN_TYPES.eof, ""),
         ]);
         expect(token.tokenize(".5")).toEqual([
-            {tokenType : "TOKEN_UNKNOWN", text : "."},
-            {tokenType : "TOKEN_NUMBER", text : "5"},
-            {tokenType : "TOKEN_EOF", text : ""},
+            new token.Token(token.TOKEN_TYPES.unknown, "."),
+            new token.Token(token.TOKEN_TYPES.number, "5"),
+            new token.Token(token.TOKEN_TYPES.eof, ""),
         ]);
         expect(token.tokenize("1...5")).toEqual([
-            {tokenType : "TOKEN_NUMBER", text : "1"},
-            {tokenType : "TOKEN_UNKNOWN", text : "."},
-            {tokenType : "TOKEN_UNKNOWN", text : "."},
-            {tokenType : "TOKEN_UNKNOWN", text : "."},
-            {tokenType : "TOKEN_NUMBER", text : "5"},
-            {tokenType : "TOKEN_EOF", text : ""},
+            new token.Token(token.TOKEN_TYPES.number, "1"),
+            new token.Token(token.TOKEN_TYPES.unknown, "."),
+            new token.Token(token.TOKEN_TYPES.unknown, "."),
+            new token.Token(token.TOKEN_TYPES.unknown, "."),
+            new token.Token(token.TOKEN_TYPES.number, "5"),
+            new token.Token(token.TOKEN_TYPES.eof, ""),
+        ]);
+    });
+
+    test("Implicit multiplication", () => {
+        expect(token.tokenize("5(5)")).toEqual([
+            new token.Token(token.TOKEN_TYPES.number, "5"),
+            new token.Token(token.TOKEN_TYPES.multiply, "*"),
+            new token.Token(token.TOKEN_TYPES.brace_left, "("),
+            new token.Token(token.TOKEN_TYPES.number, "5"),
+            new token.Token(token.TOKEN_TYPES.brace_right, ")"),
+            new token.Token(token.TOKEN_TYPES.eof, ""),
+        ]);
+
+        expect(token.tokenize("2#2")).toEqual([
+            new token.Token(token.TOKEN_TYPES.number, "2"),
+            new token.Token(token.TOKEN_TYPES.multiply, "*"),
+            new token.Token(token.TOKEN_TYPES.sqrt, "#"),
+            new token.Token(token.TOKEN_TYPES.number, "2"),
+            new token.Token(token.TOKEN_TYPES.eof, ""),
+        ]);
+
+        expect(token.tokenize("22(6)")).toEqual([
+            new token.Token(token.TOKEN_TYPES.number, "22"),
+            new token.Token(token.TOKEN_TYPES.multiply, "*"),
+            new token.Token(token.TOKEN_TYPES.brace_left, "("),
+            new token.Token(token.TOKEN_TYPES.number, "6"),
+            new token.Token(token.TOKEN_TYPES.brace_right, ")"),
+            new token.Token(token.TOKEN_TYPES.eof, ""),
+        ]);
+    });
+
+    test("Every tokens", () => {
+        expect(token.tokenize("1+-\u00D7*\u00F7/^\u221A#mod()")).toEqual([
+            new token.Token(token.TOKEN_TYPES.number, "1"),
+            new token.Token(token.TOKEN_TYPES.plus, "+"),
+            new token.Token(token.TOKEN_TYPES.minus, "-"),
+            new token.Token(token.TOKEN_TYPES.multiply, "\u00D7"),
+            new token.Token(token.TOKEN_TYPES.multiply, "*"),
+            new token.Token(token.TOKEN_TYPES.divide, "\u00F7"),
+            new token.Token(token.TOKEN_TYPES.divide, "/"),
+            new token.Token(token.TOKEN_TYPES.exponent, "^"),
+            new token.Token(token.TOKEN_TYPES.sqrt, "\u221A"),
+            new token.Token(token.TOKEN_TYPES.sqrt, "#"),
+            new token.Token(token.TOKEN_TYPES.mod, "mod"),
+            new token.Token(token.TOKEN_TYPES.brace_left, "("),
+            new token.Token(token.TOKEN_TYPES.brace_right, ")"),
+            new token.Token(token.TOKEN_TYPES.eof, "")
         ]);
     });
 });

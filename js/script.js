f16c62db016fc75ec88ecf0b9da09dba5896624b
Arnaud Sanchez <arnaudsanchez1816@gmail.com>
2025-01-13 02:14:03 +0100

    Improve clear behavior.
    Style calculator by copying the GNOME calculator style.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 import ParserModule from './parser.js';
+import LexerModule from './lexer.js';
 
 const buttons = Array.from(document.querySelectorAll("button"));
 const inputField = document.querySelector("#inputField");
 const parser = new ParserModule.Parser();
+let errorDisplayed = false;
 
 document.addEventListener("keydown", function(event) {
     const key = event.key;
@@ -17,6 +19,13 @@ buttons.forEach(x => {
 });
 
 function processKey(key) {
+    // If an error was displayed, reset the text content
+    if(errorDisplayed) {
+        inputField.textContent = "";
+        errorDisplayed = false;
+    }
+
+    const inputFieldContent = inputField.textContent;
     if(key === "Enter") {
         try {
             const expression = parser.parseString(inputField.textContent);
@@ -26,6 +35,7 @@ function processKey(key) {
         catch (exception) {
             console.error(exception);
             inputField.textContent = "Error";
+            errorDisplayed = true;
         }
         return true;
     }
@@ -34,15 +44,41 @@ function processKey(key) {
         return true;
     }
     else if(key === "Backspace") {
-        const str = inputField.textContent;
-        if(str.length > 0) {
-            inputField.textContent = str.slice(0, str.length - 1);
+        if(inputFieldContent.length <= 0) {
+            return;
         }
+        // Remove last input tokens
+        const tokens = LexerModule.tokenize(inputFieldContent, false);
+        // Pop the EOF token
+        tokens.pop();
+        
+        if(tokens[tokens.length-1].tokenType === LexerModule.TOKEN_TYPES.number) {
+            // If the last token is a number, remove the last input character instead
+            inputField.textContent = inputFieldContent.slice(0, inputFieldContent.length - 1);
+            return;
+        }
+        
+        // Remove last token and reconstruct the input text
+        tokens.pop();
+        const inputText = tokens.reduce((resultString, currentToken) => {
+            return resultString += currentToken.text;
+        }, "");
+        inputField.textContent = inputText
         return true;
     }
 
     const button = buttons.find(x => x.dataset.key === key);
     if(button) {
+        if(inputFieldContent === "0") {
+            if(key === inputFieldContent) {
+                return;
+            }
+            // Clear initial 0 if input a number something other than 0
+            if(key >= "0" && key <= "9") {
+                inputField.textContent = "";
+            }
+        }
+
         inputField.textContent += button.textContent;
         return true;
     }
